d5fb312833d40fe95289e5bc7ae37c4105a69f53
Hk669 <hrushi669@gmail.com>
2023-07-13 22:47:57 +0530

    Streamlit app

---
+++ OSrecommender.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from user_data import get_repos
+from db import recommend
+import asyncio
+from linkpreview import link_preview
+
+
+def get_link_preview(url):
+    preview = link_preview(url)
+    return preview.title, preview.description, preview.image
+
+# Application Design
+
+st.title("Open-Source Recommender")
+prompt = st.text_input("Enter your Github username..")
+
+if prompt:
+    status_placeholder = st.empty()
+    status_placeholder.text('Crawling your repositories...')
+    user_details, languages_topics = get_repos(prompt)
+
+    status_placeholder.text('Crawling open source projects...')
+    unique_repos = asyncio.run(get_projects(languages_topics))
+
+    status_placeholder.text('Generating recommendations...')
+
+    urls = recommend(user_details, unique_repos)
+
+    status_placeholder.empty()
+
+    with st.expander("Recommended Projects"):
+        for url in urls:
+            if url:
+                title, description, image = get_link_preview(url)
+
+                if image:
+                    st.markdown(f'[<img src="{image}" width="300" align="center"/>]({url})', unsafe_allow_html=True)
+                else:
+                    st.markdown(f'[{title}]({url})', unsafe_allow_html=True)
