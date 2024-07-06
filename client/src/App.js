1076a2307ecbfc46cd50e8b6de997db0dc272dbe
Hk669 <hrushi669@gmail.com>
2024-07-06 20:10:36 +0530

    fix: few styles and logic

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,40 @@
 import React, { useState } from "react";
-import axios from "axios";
 import "./App.css";
+import Input from "./components/Input/Input";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Recommendation } from "./components/Recommendation/Recommendation";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [languages, setLanguages] = useState("");
-  const [extraTopics, setExtraTopics] = useState("");
   const [recommendations, setRecommendations] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  console.log(languages)
-  console.log(extraTopics)
-  
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(
-        "http://localhost:8000/api/recommendations/",
-        {
-          username: username,
-          languages: languages.split(",").map((lang) => lang.trim()),
-          extra_topics: extraTopics.split(",").map((topic) => topic.trim()),
-        }
-      );
-      setRecommendations(response.data.recommendations);
-    } catch (error) {
-      console.error("There was an error fetching recommendations:", error);
-    }
+  const handleSubmit = async (recommendations) => {
+    setLoading(true);
+    console.log("recommendations");
+    console.log(recommendations);
+
+    setRecommendations(recommendations);
+    setLoading(false);
   };
 
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Repository Recommendations</h1>
-      </header>
-      <form onSubmit={handleSubmit}>
-        <label>
-          GitHub Username:
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Languages (comma-separated):
-          <input
-            type="text"
-            value={languages}
-            onChange={(e) => setLanguages(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Extra Topics (comma-separated):
-          <input
-            type="text"
-            value={extraTopics}
-            onChange={(e) => setExtraTopics(e.target.value)}
-          />
-        </label>
-        <br />
-        <button type="submit">Get Recommendations</button>
-      </form>
-      <div>
-        <h2>Recommendations:</h2>
-        <ul>
-          {recommendations.map((url, index) => (
-            <li key={index}>
-              <a href={url}>{url}</a>
-            </li>
-          ))}
-        </ul>
+      <Navbar />
+      <div className="App-main">
+        <div className="Input">
+          <Input onSubmit={handleSubmit} />
+        </div>
+        {loading ? (
+          <p>Loading recommendations...</p>
+        ) : recommendations.length > 0 ? (
+          <div className="Recommendation">
+            <Recommendation recommendations={recommendations} />
+          </div>
+        )
+          : null}
+        <ToastContainer />
       </div>
     </div>
   );
